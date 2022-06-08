2b68e555868a919d610532d1f10c7e3c70cd3ab3
plusdojo2022kozarashi <103239247+plusdojo2022kozarashi@users.noreply.github.com>
2022-06-08 15:50:45 +0900

    日付、ミッション追加

--- src/src/model/Date.java
+++ src/src/model/Date.java
@@ -1,5 +1,21 @@
 package model;
 
-public class Date {
+import java.io.Serializable;
 
+public class Date implements Serializable {
+	  private int date_id;
+	  private Date date;
+	public Date(int date_id, Date date) {
+		super();
+		this.date_id = date_id;
+		this.date = date;
+	}
+
+	public Date() {
+		super();
+		this.date_id = 0;
+		this.date = new Date();
+	}
 }
+
+
--- src/src/model/Mission.java
+++ src/src/model/Mission.java
@@ -1,5 +1,29 @@
 package model;
+import java.io.Serializable;
 
-public class Mission {
-
+public class Mission implements Serializable {
+	 private int mission_id;
+	  private String mission;
+	public Mission(int mission_id, String mission) {
+		super();
+		this.mission_id = mission_id;
+		this.mission = mission;
+	}
+	public Mission() {
+		super();
+		this.mission_id = 0;
+		this.mission = "";
+	}
+	public int getMission_id() {
+		return mission_id;
+	}
+	public void setMission_id(int mission_id) {
+		this.mission_id = mission_id;
+	}
+	public String getMission() {
+		return mission;
+	}
+	public void setMission(String mission) {
+		this.mission = mission;
+	}
 }

def89007383bc67926202301f3219e836674ad03
plusdojo2022kozarashi <103239247+plusdojo2022kozarashi@users.noreply.github.com>
2022-06-08 16:47:07 +0900

    バトン

--- src/src/dao/DateDao.java
+++ src/src/dao/DateDao.java
@@ -1,5 +1,34 @@
 package dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Date;
 
-public class DateDao {
+public class DateDAO {
+	// 引数paramで検索項目を指定し、検索結果のリストを返す
+	public Date select(Date param) {
+		Connection conn = null;
+		Date date = new Date();
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/simpleBC", "sa", "");
+			// SQL文を準備する
+			String sql = "select datetime from date ORDER BY date_id desc limit 1";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+			rs.next();
+
+		}
+
+	}
 
 }
